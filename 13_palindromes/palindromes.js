6fc6eade7ca7a17e49dd21c9d939d4a3dd4f59ce
Sebastian Konieczny <s.konieczny3211@gmail.com>
2025-10-24 19:22:34 +0200

    Add solutions for exercises 13-15

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    const skip = [".", "!", " ", ","];
 
+    const cleaned = sentence
+                        .toLowerCase()
+                        .split("")
+                        .filter((letter) => !skip.includes(letter))
+                        .join("");
+
+    const reversed = cleaned
+                        .split("")
+                        .reverse()
+                        .join("");
+
+    return reversed === cleaned;
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n === 0) return 0;
+    if (n < 0) return "OOPS";
+    if (n === 1 || n === 2) return 1;
 
+    return fibonacci(n-1) + fibonacci(n-2);
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
